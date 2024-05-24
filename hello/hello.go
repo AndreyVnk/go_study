6209f58afbbc803c7d4d71a54daae5b00bcf01c3
Andrei Bychkov <andrei.bychkov.vnk@gmail.com>
2024-05-24 17:02:42 +0400

    Return greetings for multiple people

--- hello/hello.go
+++ hello/hello.go
@@ -18,16 +18,27 @@ func main() {
     	log.SetFlags(0)
 	
 	// Request a greeting message.
-    	message, err := greetings.Hello("Sherry")
+    	// message, err := greetings.Hello("Sherry")
     	// If an error was returned, print it to the console and
     	// exit the program.
-    	if err != nil {
+    	
+	// A slice of names.
+    	names := []string{"Gladys", "Samantha", "Darrin"}
+
+    	// Request greeting messages for the names.
+    	messages, err := greetings.Hellos(names)
+
+	if err != nil {
         	log.Fatal(err)
     	}
     	
+    	// If no error was returned, print the returned map of
+    	// messages to the console.
+    	fmt.Println(messages)
+
 	// If no error was returned, print the returned message
     	// to the console.
-    	fmt.Println(message)
+    	// fmt.Println(message)
 
 	// Get a greeting message and print it.
     	// message := greetings.Hello("Gladys")
